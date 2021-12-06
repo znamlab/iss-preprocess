15bdb089bbe178e47b04577bd4e28bcc4735a1a2
Petr Znamenskiy <petr.znamenskiy@crick.ac.uk>
2021-12-06 17:51:19 +0000

    add `register_tracks` for aligning two tracks for the same sequencing round

--- iss_preprocess/reg/rounds.py
+++ iss_preprocess/reg/rounds.py
@@ -31,3 +31,46 @@ def register_rounds(stacks, ch_to_align=0):
         stacks[:,:,:,channel] = sr.transform_stack(stacks[:,:,:,channel].squeeze())
 
     return stacks
+
+
+def register_tracks(track1, track2, chs_to_align=(0,0)):
+    """
+    Register imaging tracks.
+
+    Args:
+        track1 (list): list of X x Y x C ndarrays for track 1 on individual rounds.
+        track2 (list): list of X x Y x C ndarrays for track 2 on individual rounds.
+        chs_to_align (tuple): channels to use for registration.
+
+    """
+    out = []
+
+    for stacks in zip(track1, track2):
+        maxx = 0
+        maxy = 0
+        for stack in stacks:
+            maxx = np.max((maxx, stack.shape[0]))
+            maxy = np.max((maxy, stack.shape[1]))
+        padded_stacks = []
+
+        for stack in stacks:
+            padx = maxx - stack.shape[0]
+            pady = maxy - stack.shape[1]
+            padded_stacks.append(np.pad(stack, ((0, padx), (0, pady), (0,0)), 'constant'))
+
+        sr = StackReg(StackReg.TRANSLATION)
+        sr.register(
+            padded_stacks[0][:,:,chs_to_align[0]].squeeze(),
+            padded_stacks[1][:,:,chs_to_align[1]].squeeze()
+        )
+
+        nchannels = padded_stacks[1].shape[2]
+        for channel in range(nchannels):
+            padded_stacks[1][:,:,channel] = sr.transform(
+                padded_stacks[1][:,:,channel]
+            )
+        # stack along the channels axis
+        stacks = np.concatenate((padded_stacks[0], padded_stacks[1]), axis=2)
+        out.append(stacks)
+
+    return out
