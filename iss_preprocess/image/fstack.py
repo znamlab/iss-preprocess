21babae7c0294824cd1f8e7c49105811130d4823
Petr Znamenskiy <petr.znamenskiy@crick.ac.uk>
2021-12-07 19:22:11 +0000

    initial version of `fstack` implementation

--- iss_preprocess/image/fstack.py
+++ iss_preprocess/image/fstack.py
@@ -22,32 +22,58 @@ def gauss3P(x, Y):
     I = np.argmax(Y, axis=2)
     Ic = I.flatten()
     Ymax = np.amax(Y, axis=2)
-    [IN,IM] = meshgrid(1:N,1:M);
-    Ic[Ic<=STEP-1] = STEP
-    Ic[Ic>=P-STEP-1] = P-STEP-1
-    Index1 = sub2ind([M,N,P], IM(:), IN(:), Ic-STEP);
-    Index2 = sub2ind([M,N,P], IM(:), IN(:), Ic);
-    Index3 = sub2ind([M,N,P], IM(:), IN(:), Ic+STEP);
+    [IN,IM] = np.meshgrid(np.arange(N), np.arange(M))
+    Ic[Ic<STEP] = STEP
+    Ic[Ic>P-STEP-1] = P-STEP-1
+    Index1 = np.ravel_multi_index([IM.flatten(), IN.flatten(), Ic-STEP], [M,N,P])
+    Index2 = np.ravel_multi_index([IM.flatten(), IN.flatten(), Ic], [M,N,P]);
+    Index3 = np.ravel_multi_index([IM.flatten(), IN.flatten(), Ic+STEP], [M,N,P])
     Index1[I.flatten()<=STEP-1] = Index3[I.flatten()<=STEP-1]
     Index3[I.flatten()>=STEP-1] = Index1[I.flatten()>=STEP-1]
     x1 = np.reshape(x[Ic-STEP],(M,N))
     x2 = np.reshape(x[Ic],(M,N))
     x3 = np.reshape(x[Ic+STEP],(M,N))
+    Y = Y.flatten()
     y1 = np.reshape(np.log(Y[Index1]),(M,N))
     y2 = np.reshape(np.log(Y[Index2]),(M,N))
-    y3 = np.reshape(np.log(Y[Index3]),M,N);
+    y3 = np.reshape(np.log(Y[Index3]),(M,N))
     c = ( (y1-y2)*(x2-x3) - (y2-y3)*(x1-x2) ) / ( (x1**2-x2**2)*(x2-x3) - (x2**2-x3**2)*(x1-x2) )
     b = ( (y2-y3)-c*(x2-x3)*(x2+x3) ) / (x2-x3)
     s = np.sqrt(-1/(2*c))
     u = b * s**2
     a = y1 - b * x1 - c * x1**2
-    A = exp(a + u**2 / (2*s**2))
+    A = np.exp(a + u**2 / (2*s**2))
     return u, s, A, Ymax
 
 def fstack(im, wsize=9, alpha=0.2, sth=13, focus=None):
-    if focus is None:
+    M, N, P = im.shape
 
+    if focus is None:
+        focus = np.arange(P)
 
     fm = np.zeros(im.shape)
-    for plane in range(im.shape[2]):
-        fn[:,:,plane] = gfocus(im[:,:,plane], wsize)
+    for plane in range(P):
+        fm[:,:,plane] = gfocus(im[:,:,plane], wsize)
+
+    u, s, A, fmax = gauss3P(focus, fm)
+
+    err = np.zeros((M, N))
+    for plane in range(P):
+        err += np.abs( fm[:,:,plane] - A * np.exp(-(focus[plane]-u)**2 / (2*s**2)))
+        fm[:,:,plane] = fm[:,:,plane] / fmax
+
+    h = np.ones((wsize, wsize)) / wsize**2
+    inv_psnr = scipy.ndimage.convolve(err / (P*fmax), h, mode='nearest')
+
+    S = 20 * np.log10(1 / inv_psnr)
+    S[np.isnan(S)] = np.min(S)
+
+    phi = 0.5 * (1+np.tanh(alpha*(S - sth))) * alpha
+    phi = scipy.signal.medfilt2d(phi, kernel_size=3)
+
+    for plane in range(P):
+        fm[:,:,plane] = 0.5 + 0.5 * np.tanh(phi * (fm[:,:,plane]-1))
+
+    fmn = np.sum(fm, axis=2)
+
+    return np.sum(im * fm / fmn[:,:,np.newaxis], axis=2)
