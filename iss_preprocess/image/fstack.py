11e2e75a83cec0999ca55431607b1de61c49f95a
Petr Znamenskiy <petr.znamenskiy@crick.ac.uk>
2021-12-07 19:37:30 +0000

    get fstack to work and add copyright notice

--- iss_preprocess/image/fstack.py
+++ iss_preprocess/image/fstack.py
@@ -1,6 +1,37 @@
 import scipy
 import numpy as np
 
+# Adapted from MATLAB implementation by S. Petruz
+#
+# Copyright (c) 2016, Said Pertuz
+# Copyright (c) 2017, Said Pertuz
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# * Redistributions of source code must retain the above copyright notice, this
+#   list of conditions and the following disclaimer.
+#
+# * Redistributions in binary form must reproduce the above copyright notice,
+#   this list of conditions and the following disclaimer in the documentation
+#   and/or other materials provided with the distribution
+# * Neither the name of Universidad Industrial de Santander nor the names of its
+#   contributors may be used to endorse or promote products derived from this
+#   software without specific prior written permission.
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+# Modified by P. Znamenskiy, 2021
+
 def gfocus(im, wsize):
     """
     Calculate focus measure as the local variance of the images
@@ -16,6 +47,8 @@ def gauss3P(x, Y):
     """
     Fast 3-point gaussian interpolation
 
+    Rewritten based on equations in "Shape from Focus", Shree K. Nayar, 1989
+
     """
     STEP = 2
     M,N,P = Y.shape
@@ -23,26 +56,27 @@ def gauss3P(x, Y):
     Ic = I.flatten()
     Ymax = np.amax(Y, axis=2)
     [IN,IM] = np.meshgrid(np.arange(N), np.arange(M))
-    Ic[Ic<STEP] = STEP
-    Ic[Ic>P-STEP-1] = P-STEP-1
-    Index1 = np.ravel_multi_index([IM.flatten(), IN.flatten(), Ic-STEP], [M,N,P])
     Index2 = np.ravel_multi_index([IM.flatten(), IN.flatten(), Ic], [M,N,P]);
-    Index3 = np.ravel_multi_index([IM.flatten(), IN.flatten(), Ic+STEP], [M,N,P])
-    Index1[I.flatten()<=STEP-1] = Index3[I.flatten()<=STEP-1]
-    Index3[I.flatten()>=STEP-1] = Index1[I.flatten()>=STEP-1]
-    x1 = np.reshape(x[Ic-STEP],(M,N))
-    x2 = np.reshape(x[Ic],(M,N))
-    x3 = np.reshape(x[Ic+STEP],(M,N))
+    Ic_1 = Ic.copy() - STEP
+    Ic_1[Ic_1<0] = Ic[Ic_1<0] + STEP
+    Ic_3 = Ic.copy() + STEP
+    Ic_3[Ic_3>=P] = Ic[Ic_3>=P] - STEP
+    Index1 = np.ravel_multi_index([IM.flatten(), IN.flatten(), Ic_1], [M,N,P])
+    Index3 = np.ravel_multi_index([IM.flatten(), IN.flatten(), Ic_3], [M,N,P])
+
+    x1 = np.reshape(Ic-STEP,(M,N))
+    x2 = np.reshape(Ic,(M,N))
+    x3 = np.reshape(Ic+STEP,(M,N))
     Y = Y.flatten()
     y1 = np.reshape(np.log(Y[Index1]),(M,N))
     y2 = np.reshape(np.log(Y[Index2]),(M,N))
     y3 = np.reshape(np.log(Y[Index3]),(M,N))
-    c = ( (y1-y2)*(x2-x3) - (y2-y3)*(x1-x2) ) / ( (x1**2-x2**2)*(x2-x3) - (x2**2-x3**2)*(x1-x2) )
-    b = ( (y2-y3)-c*(x2-x3)*(x2+x3) ) / (x2-x3)
-    s = np.sqrt(-1/(2*c))
-    u = b * s**2
-    a = y1 - b * x1 - c * x1**2
-    A = np.exp(a + u**2 / (2*s**2))
+    u = ( (y2-y3) * (x2**2 - x1**2) - (y2 - y1) * (x2**2 - x3**2) ) / \
+        ( 2 * (x3 - x2) * ( (y2 - y1) + (y2 - y3) ) )
+    s = np.sqrt(- ( (x2**2 - x1**2) + (x2**2 - x3**2 ) ) / \
+        ( 2 * ((y2-y1) + (y2-y3)) ))
+
+    A = y2 / np.exp(-(x2-u)**2 / (2*s**2) )
     return u, s, A, Ymax
 
 def fstack(im, wsize=9, alpha=0.2, sth=13, focus=None):
