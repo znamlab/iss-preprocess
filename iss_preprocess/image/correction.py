76636243dcf417d052f7c0c5fbc8cb8f52b8ebc8
Petr Znamenskiy <petr.znamenskiy@crick.ac.uk>
2021-12-13 18:03:58 +0000

    add option to correct offset based on stack metadata

--- iss_preprocess/image/correction.py
+++ iss_preprocess/image/correction.py
@@ -1,23 +1,38 @@
 import numpy as np
 from sklearn.mixture import GaussianMixture
 
-def correct_offset(tiles, n_components=5, method='min'):
+def correct_offset(tiles, method='metadata', metadata=None, n_components=5):
     """
     Estimate image offset for each channel as the minimum value or using a
     Gaussian mixture model and substract it from input images.
 
+    Args:
+        tiles (DataFrame): individual tiles
+        method (str): method for determining the offset, one of either:
+            `metadata`: uses the values recorded in the image metadata
+            `min`: uses the minimum for each channel
+            `gmm`: fits a Gaussian mixture model and uses the smallest mean
+        metadata (ElementTree): XML element tree with
+
     """
+    if metadata:
+        channels_metadata = metadata.findall(
+            './Metadata/Information/Image/Dimensions/Channels/Channel'
+        )
+
     channels = tiles.C.unique()
     for channel in channels:
         this_channel = tiles[(tiles['C'] == channel) & (tiles['Z']==0)]['data']
         data = np.concatenate(this_channel.to_numpy()).reshape(-1, 1)
-        if method=='min':
+        if method=='metadata' and metadata:
+            offset = float(channels_metadata[channel].find('./DetectorSettings/Offset').text)
+        elif method=='min':
             offset = np.min(data)
         else:
             gm = GaussianMixture(n_components=n_components, random_state=0).fit(
                 data[:10:,:]
             )
             offset = np.min(gm.means_)
-        v = tiles[tiles['C'] == channel]['data'].transform(lambda x: x-offset)
+        v = tiles[tiles['C'] == channel]['data'].transform(lambda x: x.astype(float)-offset)
         tiles.update(v)
-    return tiles
+    return tiles    
