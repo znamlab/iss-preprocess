3cdd66f18f3dc7fb202376061ab385511642f224
Petr Znamenskiy <petr.znamenskiy@crick.ac.uk>
2022-01-04 17:18:29 +0000

    Update `omp_example.py`.

--- examples/omp_example.py
+++ examples/omp_example.py
@@ -1,13 +1,23 @@
 # Initial registration
 #%%
 import pandas as pd
-
 import iss_preprocess as iss
 import numpy as np
+import matplotlib.pyplot as plt
+
 from pathlib import Path
 from skimage.io import ImageCollection
+from itertools import compress, cycle
 
 #%%
+ops = {
+    'tile_reg_pix': 200,            # number of border pixels to use for stitching
+    'ch_to_align': 0,               # channel to use for registration between rounds
+    'spot_threshold': 20,           # threshold for initial spot detection
+    'omp_tol': 0.1,                 # tolerance threshold for OMP algorithm
+    'include_cycles': np.arange(7)  # cycles to include for basecalling and OMP
+}
+
 data_dir = Path('/camp/lab/znamenskiyp/home/shared/projects/rabies_BARseq/BRAC6246.1b/brain/slide_006/section_02/sequencing_cycles')
 fnames = [
     'section_02_cycle_01.czi',
@@ -24,16 +34,19 @@ stacks = []
 for i, fname in enumerate(fnames):
     tiles, metadata = iss.io.get_tiles(data_dir / fname)
     tiles = iss.image.correct_offset(tiles, method='metadata', metadata=metadata)
-    im, tile_pos = iss.reg.register_tiles(tiles, reg_pix=200)
+    im, tile_pos = iss.reg.register_tiles(tiles, reg_pix=ops['tile_reg_pix'])
     im = iss.io.reorder_channels(im, metadata)
     stacks.append(np.mean(im, axis=3))
 
 # register sequencing rounds
 corrected_stacks = iss.image.correct_levels(stacks, stacks[0][:,:,0], method='histogram')
-corrected_stacks = iss.reg.register_rounds(corrected_stacks, ch_to_align=0)
+corrected_stacks = iss.reg.register_rounds(corrected_stacks, ch_to_align=ops['ch_to_align'])
 # save output
 for i in range(corrected_stacks.shape[0]):
-    iss.io.write_stack(corrected_stacks[i,:,:,:].squeeze(), f'/camp/home/znamenp/home/users/znamenp/tmp/stack_mean_{i}.tif')
+    iss.io.write_stack(
+        corrected_stacks[i,:,:,:].squeeze(),
+        f'/camp/home/znamenp/home/users/znamenp/tmp/stack_mean_{i}.tif'
+    )
 
 #%%
 # Load preregistered stack
@@ -41,40 +54,41 @@ coll = ImageCollection('/camp/home/znamenp/home/users/znamenp/tmp/stack_mean_*.t
 stack = coll.concatenate()
 stack = np.moveaxis(stack, 0, 2)
 stack = stack.reshape([stack.shape[0], stack.shape[1], -1, 4])
-
-stack = iss.reg.register_rounds([ stack[:1024,:1024,i,:].squeeze() for i in range(8) ], ch_to_align=0)
-stack = np.moveaxis(stack, 0, 2)
+# do fine local registration
+stack = iss.reg.register_rounds_fine(stack)
 
 #%%
 # Run OMP on the substack
-spots = iss.segment.detect_spots(stack[:,:,0,:].squeeze(), method='trackpy', threshold=20)
-rois = iss.call.extract_spots(spots, stack[:,:,:7,:])
-
-codebook = pd.read_csv('../iss_preprocess/call/codebook_YS220.csv', header=0, names=['gii', 'seq', 'gene'])
-gene_dict, unique_genes = iss.call.make_gene_templates(rois, codebook)
-g, b, r = iss.call.run_omp(stack[:,:,:7,:], gene_dict)
+spots = iss.segment.detect_spots(
+    stack[:, :, 0, :].squeeze(),
+    method='trackpy',
+    threshold=ops['spot_threshold']
+)
+rois = iss.call.extract_spots(spots, stack[:, :, ops['include_cycles'], :])
+# temporarily needed to avoid ROIs that have 0s for all channels in some rounds
+valid_rois = np.mean(iss.call.rois_to_array(rois).reshape((-1, len(rois))), axis=0) > 0
+codebook = pd.read_csv(
+    '../iss_preprocess/call/codebook_YS220.csv',
+    header=0,
+    names=['gii', 'seq', 'gene']
+)
+gene_dict, unique_genes = iss.call.make_gene_templates(list(compress(rois, valid_rois)), codebook)
+# trimming stack to avoid pixels that are always zero
+# OMP functions should check for this in the future
+g, b, r = iss.call.run_omp(
+    stack[:7000, :6900, ops['include_cycles'], :],
+    gene_dict,
+    tol=ops['omp_tol']
+)
 
 #%%
-import matplotlib.pyplot as plt
-from skimage.feature import blob_log
-from scipy.signal import medfilt2d
-from itertools import cycle
-
+# Detect and plot genes
 markers = cycle('ov^<>spPXD*')
 plt.figure(figsize=(15,15))
 ax = plt.subplot(1,1,1)
 h = []
 for gene_idx, gene in enumerate(unique_genes):
-    spots_array = blob_log(
-        medfilt2d(g[:,:,gene_idx], kernel_size=3),
-        max_sigma=4.,
-        min_sigma=.5,
-        num_sigma=10,
-        log_scale=True,
-        overlap=0.9,
-    )
-    gene_spots = pd.DataFrame(spots_array, columns=['y', 'x', 'size'])
-    gene_spots = gene_spots[gene_spots['size']>=1.2]
+    gene_spots = iss.segment.detect_gene_spots(g[:,:,gene_idx])
     h.append(
         plt.plot(gene_spots['x'], gene_spots['y'], next(markers))
     )
