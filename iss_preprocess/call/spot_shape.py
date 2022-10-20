cbeb00a4d178e91a96d8be52ec3b501804da90ec
Petr Znamenskiy <petr.znamenskiy@crick.ac.uk>
2022-10-20 18:16:15 +0100

    add gene spot datection

--- iss_preprocess/call/spot_shape.py
+++ iss_preprocess/call/spot_shape.py
@@ -3,7 +3,6 @@ import numpy as np
 from ..segment import detect_spots
 from math import floor
 
-
 def get_spot_shape(g, spot_xy=7, neighbor_filter_size=9, neighbor_threshold=15):
     spot_sign_image = np.zeros((spot_xy*2 + 1, spot_xy*2 + 1))
     nspots = 0
@@ -38,3 +37,24 @@ def apply_symmetry(spot_sign_image):
     for unique_d in unique_ds:
         symmetric_spot_sign_image[D == unique_d] = np.mean(spot_sign_image[D == unique_d])
     return symmetric_spot_sign_image
+
+
+def find_gene_spots(g, spot_sign_image, rho=2, omp_score_threshold=0.05):
+    neg_max = np.sum(np.sign(spot_sign_image) == -1)
+    pos_max = np.sum(np.sign(spot_sign_image) == 1)
+    ngenes = g.shape[2]
+    all_genes = []
+    for igene in range(ngenes):
+        print(f'findings spots for gene {igene} of {ngenes}...')
+        gene_spots = detect_spots(g[:,:,igene], method='dilation', threshold=0)
+        pos_filter = (np.sign(spot_sign_image) == 1).astype(float)
+        neg_filter = (np.sign(spot_sign_image) == -1).astype(float)
+        gene_filt_pos = cv2.filter2D((g[:,:,igene]>0).astype(float), -1, pos_filter, borderType=cv2.BORDER_REPLICATE)
+        gene_filt_neg = cv2.filter2D((g[:,:,igene]<0).astype(float), -1, neg_filter, borderType=cv2.BORDER_REPLICATE)
+        pos_pixels = gene_filt_pos[gene_spots['y'], gene_spots['x']]
+        neg_pixels = gene_filt_neg[gene_spots['y'], gene_spots['x']]
+        omp_score = (neg_pixels + pos_pixels * rho) / (neg_max + pos_max * rho)
+        gene_spots['omp_score'] = omp_score
+        gene_spots = gene_spots.iloc[omp_score > omp_score_threshold]
+        all_genes.append(gene_spots)
+    return all_genes
