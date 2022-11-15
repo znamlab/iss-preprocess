ddc74d975ae8d98ee6fb774af8dde762dd24ab27
Petr Znamenskiy <petr.znamenskiy@crick.ac.uk>
2022-11-15 14:09:51 +0000

    pipeline code for multitile projection

--- iss_preprocess/pipeline/pipeline.py
+++ iss_preprocess/pipeline/pipeline.py
@@ -1,26 +1,16 @@
 import numpy as np
 import pandas as pd
+import glob
+import multiprocessing as mp
+from flexiznam.config import PARAMETERS
+from pathlib import Path
+from os.path import isfile
+
 from ..image import correct_offset, fstack_channels, filter_stack
 from ..reg import register_tiles
 from ..io import load_stack, get_tiles, get_tile_ome, reorder_channels, write_stack
 from ..segment import detect_isolated_spots
 from ..call import extract_spots, make_gene_templates
-
-
-def load_image(fname, ops):
-    tiles, metadata = get_tiles(fname)
-    tiles = correct_offset(tiles, method='metadata', metadata=metadata)
-    im, tile_pos = register_tiles(
-        tiles,
-        reg_fraction=ops['tile_reg_fraction'],
-        method='custom',
-        max_shift=ops['max_shift'],
-        ch_to_align=-1
-    )
-    im = reorder_channels(im, metadata)
-    if im.ndim>3:
-        im = np.mean(im, axis=3)
-    return im
 
 
 def setup_omp(fname, nchannels=4, nrounds=7):
@@ -54,11 +44,91 @@ def setup_omp(fname, nchannels=4, nrounds=7):
     return gene_dict, unique_genes, norm_shift
 
 
-def project_tile(fnames):
-    for fname in fnames:
-        print(f'loading {fname}')
-    im = get_tile_ome(fname + '.ome.tif', fname + '_metadata.txt')
-    print('computing projection')
-    im_proj = fstack_channels(im, sth=10)
-    #im_proj = np.max(im, axis=3)
-    write_stack(im_proj, fname + '_proj.tif', bigtiff=True)
+def check_files(data_path, nrounds=7):
+    """Check that TIFFs are present for all imaging rounds and return their list
+
+    Args:
+        data_path (str): relative path to the raw data
+        nrounds (int, optional): number of sequencing rounds to look for
+
+    Returns:
+        bool: whether matching TIFFs for found for all rounds
+        list: list of tiff paths for round 1
+
+    """
+    raw_path = Path(PARAMETERS['data_root']['raw'])
+    data_path = raw_path / data_path
+
+    tiffs = sorted(glob.glob(data_path / 'round_01_1/*.tif'))
+    success = True
+    # check that all files exist
+    for iround in range(nrounds):
+        for tiff in tiffs:
+            fname = tiff.replace('round_01', f'round_{str(iround+1).zfill(2)}')
+            if not isfile(fname):
+                print(f'{fname} does not exist')
+                success = False
+    return success, tiffs
+
+
+def project_tile(fname):
+    """Calculates extended depth of field and max intensity projections for a single tile.
+
+    Args:
+        fname (str): path to tile *without* `'.ome.tif'` extension.
+
+    """
+    raw_path = Path(PARAMETERS['data_root']['raw'])
+    processed_path = Path(PARAMETERS['data_root']['processed'])
+    print(f'loading {fname}\n')
+    im = get_tile_ome(raw_path / (fname + '.ome.tif'), raw_path / (fname + '_metadata.txt'))
+    print('computing projection\n')
+    im_fstack = fstack_channels(im, sth=8)
+    im_max = np.max(im, axis=3)
+    (processed_path / fname).parent.mkdir(parents=True, exist_ok=True)
+    write_stack(im_fstack, processed_path / (fname + '_fstack.tif'), bigtiff=True)
+    write_stack(im_max, processed_path / (fname + '_max.tif'), bigtiff=True)
+
+
+def load_processed_tile(data_path, tile_coors=(1,0,0), nrounds=7, suffix='_proj'):
+    """_summary_
+
+    Args:
+        data_path (str): relative path to dataset.
+        tile_coors (tuple, optional): Coordinates of tile to load: ROI, Xpos, Ypos. 
+            Defaults to (1,0,0).
+        nrounds (int, optional): Number of rounds to load. Defaults to 7.
+        suffix (str, optional): File name suffix. Defaults to '_proj'.
+
+    Returns:
+        numpy.ndarray: X x Y x channels x rounds stack.
+
+    """
+    tile_roi, tile_x, tile_y = tile_coors
+    processed_path = Path(PARAMETERS['data_root']['processed'])
+    ims = []
+    for iround in range(nrounds):
+        fname = f'round_{str(iround+1).zfill(2)}_1_MMStack_{tile_roi}-' + \
+            f'{str(tile_x).zfill(3)}_{str(tile_y).zfill(3)}_{suffix}.tif'
+        ims.append(load_stack(processed_path / data_path / fname))
+    return np.stack(ims, axis=3)
+
+
+def preprocess_images(data_path, nrounds=7):
+    success, tiffs = check_files(data_path, nrounds=nrounds)
+    if not success:
+        print('Some files were missing... aborting...')
+        return
+
+    fnames = []
+    for tiff in tiffs:
+        for iround in range(nrounds):
+            fname = tiff \
+                .replace('round_01', f'round_{str(iround+1).zfill(2)}') \
+                .replace('.ome.tif', '')
+            fnames.append(str(Path(fname).relative_to(PARAMETERS['data_root']['raw'])))
+
+    max_workers = 16
+    pool = mp.Pool(np.min((mp.cpu_count(), max_workers)))
+    results = pool.map(project_tile, fnames)
+    pool.close()
