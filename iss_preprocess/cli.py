1f734dd3444cbfca79bd92ecb9f6a8267fd08a92
Petr Znamenskiy <petr.znamenskiy@crick.ac.uk>
2022-12-09 15:44:16 +0000

    batch scripts for running registration

--- iss_preprocess/cli.py
+++ iss_preprocess/cli.py
@@ -13,12 +13,17 @@ def cli():
 )
 @click.option("-x", default=0, help="Tile X position")
 @click.option("-y", default=0, help="Tile Y position.")
-@click.option("--save", is_flag=True, show_default=True, default=False,
-    help="Whether to save registered tile images.")
+@click.option(
+    "--save",
+    is_flag=True,
+    show_default=True,
+    default=False,
+    help="Whether to save registered tile images.",
+)
 def extract_tile(path, roi=1, x=0, y=0, save=False):
+    """Run OMP and a single tile and detect gene spots."""
     from iss_preprocess.pipeline import run_omp_on_tile
 
-    """Run OMP and a single tile and detect gene spots."""
     click.echo(f"Processing ROI {roi}, tile {x}, {y} from {path}")
     run_omp_on_tile(path, (roi, x, y), save_stack=save, correct_channels=True)
 
@@ -41,9 +46,9 @@ def extract_tile(path, roi=1, x=0, y=0, save=False):
     help="Whether to overwrite tiles if already projected.",
 )
 def project_tile(path, prefix, roi=1, x=0, y=0, overwrite=False):
+    """Calculate Z-projection on a single tile."""
     from iss_preprocess.pipeline import project_tile_by_coors
 
-    """Run OMP and a single tile and detect gene spots."""
     click.echo(f"Projecting ROI {roi}, {prefix}, tile {x}, {y} from {path}")
     project_tile_by_coors((roi, x, y), path, prefix, overwrite=overwrite)
 
@@ -66,9 +71,9 @@ def project_tile(path, prefix, roi=1, x=0, y=0, overwrite=False):
     help="Whether to overwrite tiles if already projected.",
 )
 def project_row(path, prefix, roi=1, x=0, max_col=0, overwrite=False):
+    """Calculate Z-projection for a single row of tiles"""
     from iss_preprocess.pipeline import project_tile_row
 
-    """Run OMP and a single tile and detect gene spots."""
     click.echo(f"Projecting ROI {roi}, {prefix}, row {x}, from {path}")
     project_tile_row(path, prefix, roi, x, max_col, overwrite=overwrite)
 
@@ -86,8 +91,51 @@ def project_row(path, prefix, roi=1, x=0, max_col=0, overwrite=False):
     help="Whether to overwrite tiles if already projected.",
 )
 def project_round(path, prefix, overwrite=False):
+    """Calculate Z-projection for all tiles in a single sequencing round."""
     from iss_preprocess.pipeline import project_round
 
-    """Run OMP and a single tile and detect gene spots."""
     click.echo(f"Projecting ROI {prefix} from {path}")
     project_round(path, prefix, overwrite=overwrite)
+
+
+@cli.command()
+@click.option("-p", "--path", prompt="Enter data path", help="Data path.")
+@click.option("-n", "--prefix", help="Path prefix, e.g. 'genes_round'")
+@click.option(
+    "-r", "--roi", default=1, prompt="Enter ROI number", help="Number of the ROI.."
+)
+@click.option("-x", "--tilex", default=0, help="Tile X position")
+@click.option("-y", "--tiley", default=0, help="Tile Y position.")
+@click.option(
+    "-s", "--suffix", default="fstack", help="Projection suffix, e.g. 'fstack'"
+)
+def register_tile(path, prefix, roi, tilex, tiley, suffix="fstack"):
+    """Estimate X-Y shifts across rounds and channels for a single tile."""
+    from iss_preprocess.pipeline import estimate_shifts_by_coors
+
+    click.echo(f"Registering ROI {roi}, tile {tilex}, {tiley} from {path}")
+    estimate_shifts_by_coors(
+        path, tile_coors=(roi, tilex, tiley), prefix=prefix, suffix=suffix
+    )
+
+
+@cli.command()
+@click.option("-p", "--path", prompt="Enter data path", help="Data path.")
+@click.option("-n", "--prefix", help="Path prefix, e.g. 'genes_round'")
+@click.option(
+    "-s", "--suffix", default="fstack", help="Projection suffix, e.g. 'fstack'"
+)
+def estimate_shifts(path, prefix, suffix="fstack"):
+    """Estimate X-Y shifts across rounds and channels for all tiles."""
+    from iss_preprocess.pipeline import estimate_shifts_all_tiles
+
+    estimate_shifts_all_tiles(path, prefix, suffix)
+
+
+@cli.command()
+@click.option("-p", "--path", prompt="Enter data path", help="Data path.")
+def correct_shifts(path):
+    """Correct X-Y shifts using robust regression across tiles."""
+    from iss_preprocess.pipeline import correct_shifts
+
+    correct_shifts(path)
