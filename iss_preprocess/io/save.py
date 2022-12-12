c14e8fd8ce33e52c2bda17f0f00009172b46c015
Antonin Blot <antonin.blot@crick.ac.uk>
2022-12-12 16:00:07 +0000

    Add a function to save pyramidal ome-tif

--- iss_preprocess/io/save.py
+++ iss_preprocess/io/save.py
@@ -1,4 +1,5 @@
 from tifffile import TiffWriter
+import cv2
 import numpy as np
 
 
@@ -18,3 +19,100 @@ def write_stack(stack, fname, bigtiff=False):
     with TiffWriter(fname, bigtiff=bigtiff) as tif:
         for frame in range(stack.shape[2]):
             tif.write(np.uint16(stack[:, :, frame]), contiguous=True)
+
+
+def save_ome_tiff_pyramid(
+    target,
+    image,
+    pixel_size,
+    subresolutions=3,
+    max_size=None,
+    dtype="uint16",
+    verbose=True,
+):
+    """Write single image plane as pyramidal ome-tiff
+
+    Args:
+        target (str): Path to file
+        image (np.array): 2D array with 8-bit image data
+        pixel_size (float): Pixel size in microns
+        subresolutions (int, optional): Number of pyramid levels. Defaults to 3.
+        max_size (float, optional): Pixel size for the biggest layer. Data will be
+            downsampled to that size first and then by a factor 2 every level. Defaults
+            to None.
+        dtype (str, optional): Image datatype, can be "uint16" or "uint8".
+            Defaults to "uint16".
+        verbose (bool, optional): Print progress. Defaults to True.
+
+    Returns:
+        np.array: Last level of the pyramid, most downsampled image
+    """
+    # images are sometimes very dark, scale them to fill the int16
+    if dtype not in ["uint8", "uint16"]:
+        raise NotImplementedError("`dtype` must be uint8 or uint16")
+    nbits = int(dtype[4:])
+    max_val = 2**nbits - 1
+    if verbose:
+        print("... Copying array")
+    image = np.clip(image, 0, max_val)  # clip to avoid overflow
+    image = np.array(image, dtype=dtype)
+
+    if max_size is not None:
+        ori_size = pixel_size
+        ratio = int(max_size / ori_size)
+        image = cv2.resize(
+            image,
+            (image.shape[1] // ratio, image.shape[0] // ratio),
+            interpolation=cv2.INTER_CUBIC,
+        )
+        pixel_size = max_size
+
+    metadata = {
+        "axes": "YX",
+        "SignificantBits": nbits,
+        "PhysicalSizeX": pixel_size,
+        "PhysicalSizeXUnit": "Âµm",
+        "PhysicalSizeY": pixel_size,
+        "PhysicalSizeYUnit": "Âµm",
+    }
+
+    with TiffWriter(target, bigtiff=True) as tif:
+        options = dict(
+            photometric="minisblack", tile=(128, 128), resolutionunit="CENTIMETER"
+        )
+        if verbose:
+            print("... writing full res image")
+        tif.write(
+            image,
+            subifds=subresolutions,
+            resolution=(1e4 / pixel_size, 1e4 / pixel_size),
+            metadata=metadata,
+            **options
+        )
+        for level in range(subresolutions):
+            if verbose:
+                print("... writing pyramidal layer %d" % (level + 1))
+
+            mag = 2 ** (level + 1)
+            image = cv2.resize(
+                image,
+                (image.shape[1] // 2, image.shape[0] // 2),
+                interpolation=cv2.INTER_LINEAR,
+            )
+            tif.write(
+                image,
+                resolution=(1e4 / mag / pixel_size, 1e4 / mag / pixel_size),
+                **options
+            )
+        if max(image.shape) < 200:
+            skip = 1
+        else:
+            skip = int(max(image.shape) / 100)
+        if dtype == "uint16":
+            # thumbnail are always uint8
+            thumbnail = (image[::skip, ::skip] >> 2).astype("uint8")
+        else:
+            thumbnail = image[::skip, ::skip]
+        # >> 2 if to shift bits before conversion to int8
+        tif.write(thumbnail, metadata={"Name": "thumbnail"})
+    return image
