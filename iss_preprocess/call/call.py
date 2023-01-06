5f83201418993dd9bfbfea76e44f9e297d653915
Petr Znamenskiy <petr.znamenskiy@crick.ac.uk>
2023-01-06 13:24:41 +0000

    improve plotting of cluster means

--- iss_preprocess/call/call.py
+++ iss_preprocess/call/call.py
@@ -18,7 +18,7 @@ def get_cluster_means(rois, vis=False, score_thresh=0):
     nrounds = x.shape[0]
     nch = x.shape[1]
     if vis:
-        _, ax1 = plt.subplots(nrows=2, ncols=ceil(nrounds / 2))
+        _, ax1 = plt.subplots(nrows=1, ncols=nch)
         _, ax2 = plt.subplots(nrows=2, ncols=ceil(nrounds / 2))
     cluster_means = []
     cluster_means = []
@@ -31,12 +31,6 @@ def get_cluster_means(rois, vis=False, score_thresh=0):
         for ich in range(nch):
             cluster_intensity[iround, ich] = np.mean(x[iround, ich, cluster_ind == ich])
         if vis:
-            plt.sca(ax1.flatten()[iround])
-            plt.imshow(norm_cluster_mean)
-            plt.xlabel("channels")
-            plt.ylabel("clusters")
-            plt.yticks(range(4))
-            plt.title(f"round {iround+1}")
             plt.sca(ax2.flatten()[iround])
             for ich in range(nch):
                 plt.plot(
@@ -52,6 +46,13 @@ def get_cluster_means(rois, vis=False, score_thresh=0):
             cluster_means[iround][ich, :] = (
                 cluster_means[iround][ich, :] * cluster_intensity[iround, ich]
             )
+    if vis:
+        for ich in range(nch):
+            plt.sca(ax1[ich])
+            plt.imshow(np.stack(cluster_means, axis=2)[ich,:,:])
+            plt.xlabel("channels")
+            plt.ylabel("rounds")
+            plt.title(f"channel {ich+1}")
     return cluster_means
 
 
@@ -241,15 +242,21 @@ def correct_barcode_sequences(spots, max_edit_distance=2):
         # if within edit distance and lower in the list (i.e. lower abundance),
         # then update the sequence
         edit_distance = np.sum((unique_sequences - sequence) != 0, axis=1)
-        sequences_to_correct = np.logical_and(edit_distance <= max_edit_distance, np.logical_not(reassigned))
-        sequences_to_correct[:i + 1] = False
+        sequences_to_correct = np.logical_and(
+            edit_distance <= max_edit_distance, np.logical_not(reassigned)
+        )
+        sequences_to_correct[: i + 1] = False
         corrected_sequences[sequences_to_correct, :] = sequence
         reassigned[sequences_to_correct] = True
 
     for original_sequence, new_sequence in zip(unique_sequences, corrected_sequences):
         if not np.array_equal(original_sequence, new_sequence):
-            sequences[np.all((sequences - original_sequence) == 0, axis=1), :] = new_sequence
+            sequences[
+                np.all((sequences - original_sequence) == 0, axis=1), :
+            ] = new_sequence
 
-    spots["corrected_sequence"] = [ seq for seq in sequences ]
-    spots["corrected_bases"] = ["".join(BASES[seq]) for seq in spots["corrected_sequence"]]
+    spots["corrected_sequence"] = [seq for seq in sequences]
+    spots["corrected_bases"] = [
+        "".join(BASES[seq]) for seq in spots["corrected_sequence"]
+    ]
     return spots
