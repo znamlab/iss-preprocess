9c15130534562b50e4b7c7119e30540f31dc7270
Antonin Blot <antonin.blot@crick.ac.uk>
2023-01-17 17:54:27 +0000

    Add diagnostics for illumination correction

---
+++ iss_preprocess/pipeline/diagnostic.py
@@ -0,0 +1,123 @@
+"""
+Module containing diagnostic plots to make sure steps of the pipeline run smoothly
+
+The functions in here do not compute anything useful, but create figures
+"""
+from pathlib import Path
+from natsort import natsorted
+import numpy as np
+import matplotlib.pyplot as plt
+import flexiznam as flz
+from iss_preprocess.io.load import load_stack
+
+
+def check_illumination_correction(
+    data_path, grand_averages=("barcode_round", "genes_round")
+):
+    """Check if illumination correction average look reasonable
+
+    Args:
+        data_path (str): Relative path to data folder
+    """
+    processed = Path(flz.PARAMETERS["data_root"]["processed"])
+    average_dir = processed / data_path / "averages"
+    diagnostic_folder = processed / data_path / "diagnostics"
+    diagnostic_folder.mkdir(exist_ok=True)
+    correction_images = dict()
+
+    for fname in average_dir.glob("*average.tif"):
+        correction_images[fname.name.replace("_average.tif", "")] = load_stack(fname)
+    print(f"Found {len(correction_images)} averages")
+    correction_images.keys()
+
+    fig = plt.figure(figsize=(10, 10))
+    for prefix in grand_averages:
+        print(f"Doing {prefix}")
+        # first plot
+        fig.clear()
+        print("    Grand average, all channels")
+        fig.suptitle(prefix)
+        grand_av = correction_images[prefix]
+        ax_ids = np.unravel_index(range(4), (2, 2))
+        axes = [
+            plt.subplot2grid(shape=(4, 3), loc=(i, j), fig=fig) for i, j in zip(*ax_ids)
+        ]
+        titles = [f"Channel {i}" for i in range(4)]
+        _plot_channels_intensity(axes, grand_av, chan_names=titles)
+        print("    Grand average, inter channel comparison")
+        axes = [
+            plt.subplot2grid(shape=(4, 3), loc=(i + 2, j), fig=fig)
+            for i, j in zip(*ax_ids)
+        ]
+        titles = [f"Ch{i} - Ch0" for i in range(4)]
+        _plot_channels_intensity(axes, grand_av, subtract_chan=0, chan_names=titles)
+        print("    Collecting single rounds")
+        all_ch0 = {}
+        for k in correction_images:
+            if (k.startswith(prefix)) and (len(k) > len(prefix)):
+                all_ch0[k] = correction_images[k][:, :, 0]
+        av_names = natsorted(all_ch0.keys())
+        all_ch0 = np.dstack([grand_av[:, :, 0]] + [all_ch0[k] for k in av_names])
+        av_names = ["Grand average"] + [
+            name.replace(prefix + "_", "") for name in av_names
+        ]
+        print("    Single rounds, channel 0")
+        axes = [
+            plt.subplot2grid(shape=(len(av_names) + 1, 6), loc=(i, 4), fig=fig)
+            for i in range(len(av_names))
+        ]
+        _plot_channels_intensity(axes, all_ch0, chan_names=av_names)
+        axes = [
+            plt.subplot2grid(shape=(len(av_names) + 1, 6), loc=(i, 5), fig=fig)
+            for i in range(len(av_names))
+        ]
+        print("    Single rounds, compare to grand average")
+        _plot_channels_intensity(axes, all_ch0, subtract_chan=0)
+        plt.subplots_adjust(wspace=0.5)
+        fig.savefig(diagnostic_folder / f"average_for_correction_{prefix}.png", dpi=600)
+
+    other_prefix = []
+    for prefix in correction_images:
+        if any([prefix.startswith(grandav) for grandav in grand_averages]):
+            continue
+        other_prefix.append(prefix)
+    fig.clear()
+    fig.set_size_inches(10, 2 * len(other_prefix) + 0.5)
+    for ip, prefix in enumerate(other_prefix):
+        axes = [fig.add_subplot(len(other_prefix), 4, ip * 4 + i + 1) for i in range(4)]
+        _plot_channels_intensity(
+            axes, correction_images[prefix], chan_names=[prefix, "", "", ""]
+        )
+    plt.subplots_adjust(wspace=0.2)
+    fig.savefig(diagnostic_folder / f"average_for_correction_other_prefix.png", dpi=600)
+
+
+def _plot_channels_intensity(
+    axes, correction_image, subtract_chan=None, chan_names=None
+):
+    """Simple subfunction to plot the 4 channels of an image
+
+    Args:
+        axes (list): List of plt.Axes to plot. Must be same lenght as number of channel
+        correction_image (np.array): A X x Y x Nch image to plot
+        subtract_chan (int, optional): Channel to subtract before displaying. Defaults
+            to None, no subtraction
+        chan_names (list, optional): list of channel names to label axes
+    """
+    assert len(axes) == correction_image.shape[-1]
+    for i, ax in enumerate(axes):
+        if subtract_chan is None:
+            img = ax.imshow(correction_image[:, :, i], vmin=0, vmax=1)
+        else:
+            subtract_chan = int(subtract_chan)
+            img = ax.imshow(
+                correction_image[:, :, i] - correction_image[:, :, 0],
+                cmap="RdBu_r",
+                vmin=-0.1,
+                vmax=0.1,
+            )
+        if chan_names is not None:
+            ax.set_ylabel(chan_names[i])
+        plt.colorbar(img)
+        ax.set_xticks([])
+        ax.set_yticks([])
