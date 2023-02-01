c0734968f2dcb34118ac0796f4bb2ce009c5e444
Antonin Blot <antonin.blot@crick.ac.uk>
2023-02-01 11:32:24 +0000

    Add diagnostic plot with overlay for registration

--- iss_preprocess/vis/diagnostics.py
+++ iss_preprocess/vis/diagnostics.py
@@ -1,6 +1,11 @@
 from natsort import natsorted
 import numpy as np
 import matplotlib.pyplot as plt
+import cv2
+from pathlib import Path
+from flexiznam import PARAMETERS
+from iss_preprocess.pipeline import stitch_tiles, register_adjacent_tiles
+from iss_preprocess.pipeline import ara_registration as ara_reg
 
 
 def plot_correction_images(
@@ -123,3 +128,68 @@ def _plot_channels_intensity(
         plt.colorbar(img)
         ax.set_xticks([])
         ax.set_yticks([])
+
+
+def plot_registration(data_path, roi, reference_prefix="genes_round_1_1"):
+    """Overlay reference image to ARA borders
+
+    Args:
+        data_path (str): Relative path to data
+        roi (int): ROI number to plot
+        reference_prefix (str, optional): Image to use as reference. Defaults to 
+            "genes_round_1_1".
+
+    Raises:
+        ImportError: If `cricksaw_analysis` is not installed
+
+    Returns:
+        plt.Figure: Reference to the figure created.
+    """
+    try:
+        from cricksaw_analysis import atlas_utils
+    except ImportError:
+        raise ImportError('`plot_registration requires `cricksaw_analysis')
+    
+    area_ids = ara_reg.make_area_image(
+        data_path=data_path, roi=roi, atlas_size=10, full_scale=False
+    )
+    reg_metadata = ara_reg.load_registration_reference_metadata(data_path, roi=roi)
+
+    processed_path = Path(PARAMETERS["data_root"]["processed"])
+    ops = np.load(processed_path / data_path / "ops.npy", allow_pickle=True).item()
+    shift_right, shift_down, tile_shape = register_adjacent_tiles(
+        data_path, ref_coors=ops["ref_tile"], prefix=reference_prefix
+    )
+
+    stitched_stack = stitch_tiles(
+        data_path,
+        reference_prefix,
+        shift_right,
+        shift_down,
+        suffix=ops["projection"],
+        roi=roi,
+        ich=ops["ref_ch"],
+        correct_illumination=True,
+    ).astype(np.single)
+
+    fig = plt.figure()
+    ax = fig.add_subplot(1, 1, 1)
+    new_shape = reg_metadata["new_shape"]
+    downsampled = cv2.resize(stitched_stack, (new_shape[1], new_shape[0]))
+    ax.imshow(
+        downsampled,
+        extent=(0, new_shape[1], 0, new_shape[0]),
+        vmax=np.quantile(downsampled, 0.99),
+        vmin=0,
+        origin="lower",
+    )
+
+    atlas_utils.plot_borders_and_areas(
+        ax,
+        area_ids,
+        areas_to_plot=[],
+        border_kwargs=dict(colors="w", alpha=0.6, linewidths=0.1),
+    )
+
+    ax.set_ylim(ax.get_ylim()[::-1])
+    return fig
