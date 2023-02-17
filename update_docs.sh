111ba30083dfc25ad7f3ad625ba15a96cc2988f5
Petr Znamenskiy <petr.znamenskiy@crick.ac.uk>
2023-02-17 10:46:22 +0000

    [docs] fix sphinx settings

--- update_docs.sh
+++ update_docs.sh
@@ -1,5 +1,6 @@
 #
 SRCDOCS=$PWD/docs/build/html
+TMPREPO=$PWD/.tmp
 
 cd $PWD/docs
 make clean
@@ -8,13 +9,12 @@ make html
 cd $SRCDOCS
 MSG="Updating docs"
 
-TMPREPO=$PWD/.tmp
 rm -rf $TMPREPO
 mkdir -p -m 0755 $TMPREPO
 
 git clone git@github.com:znamlab/iss-preprocess.git $TMPREPO
 cd $TMPREPO
 git checkout gh-pages  ###gh-pages has previously one off been set to be nothing but html
-cp -r $SRCDOCS/ $TMPREPO
+cp -r $SRCDOCS/* $TMPREPO
 git add -A
 git commit -m "$MSG" && git push origin gh-pages
