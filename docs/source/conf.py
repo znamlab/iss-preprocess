e37a7bd015352781900763e9ad9239b39e23fcc1
Petr Znamenskiy <petr.znamenskiy@crick.ac.uk>
2023-05-09 08:38:00 +0100

    Increment version to 0.2.1

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = "2023, Znamenskiy lab"
 author = "Znamenskiy lab"
 
 # The full version, including alpha/beta/rc tags
-release = "0.2"
+release = "0.2.1"
 
 
 # -- General configuration ---------------------------------------------------
