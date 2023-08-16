b61b0ba5b29502a3e98e5ef8384a1804288fc79e
Petr Znamenskiy <petr.znamenskiy@crick.ac.uk>
2023-08-16 08:59:15 +0100

    [chore] increment version to 0.2.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="iss_preprocess",
-    version="v0.2.2",
+    version="v0.2.3",
     url="https://github.com/znamlab/iss-preprocess",
     license="MIT",
     author="Znamenskiy lab",
