ac30d11575dd29ad09fbcbfa07de35a0737f89b2
Antonin Blot <antonin.blot@crick.ac.uk>
2023-11-28 17:06:15 +0000

    [feature] apply median filter before ref tile reg

--- iss_preprocess/pipeline/register.py
+++ iss_preprocess/pipeline/register.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 import iss_preprocess as iss
 from sklearn.linear_model import RANSACRegressor
+from scipy.ndimage import median_filter
 from ..reg import (
     register_channels_and_rounds,
     estimate_shifts_for_tile,
@@ -34,6 +35,14 @@ def register_reference_tile(data_path, prefix="genes_round"):
     stack = load_sequencing_rounds(
         data_path, ops["ref_tile"], prefix=prefix, suffix=projection, nrounds=nrounds
     )
+    if ops["reg_median_filter"]:
+        msize = ops["reg_median_filter"]
+        print(f"Filtering with median filter of size {msize}")
+        assert isinstance(msize, int), "reg_median_filter must be an integer"
+        for ch in range(stack.shape[2]):
+            for r in range(stack.shape[3]):
+                stack[:, :, ch, r] = median_filter(stack[:, :, ch, r], size=msize)
+
     (
         angles_within_channels,
         shifts_within_channels,
