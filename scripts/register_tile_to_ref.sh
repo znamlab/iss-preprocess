8dcce13e579c1c8e0958f7ab4914f7ec8064e03c
Antonin Blot <antonin.blot@crick.ac.uk>
2024-04-22 15:41:34 +0100

    [minor] make script more verbose
    To debug where slurm fails when it doesn't start the python script

--- scripts/register_tile_to_ref.sh
+++ scripts/register_tile_to_ref.sh
@@ -4,14 +4,31 @@
 #SBATCH --time=30:00
 #SBATCH --mem=16G
 #SBATCH --partition=ncpu
+
+echo "Starting job $SLURM_JOB_ID"
+echo "Running on $SLURM_JOB_NODELIST"
+echo "Starting register_tile_to_ref.sh"
+echo "Parameters:"
+echo "  DATAPATH: $DATAPATH"
+echo "  REG_PREFIX: $REG_PREFIX"
+echo "  ROI: $ROI"
+echo "  TILEX: $TILEX"
+echo "  TILEY: $TILEY"
+
+echo "Loading modules"
 ml purge
 
 ml Anaconda3
-source activate base
 
+echo "Setting up conda environment"
+source activate base
 conda activate iss-preprocess
+
+echo "Running script"
 if $USE_MASK; then
+    echo "Using mask"
     iss register-to-reference -p $DATAPATH -n $REG_PREFIX -r $ROI -x $TILEX -y $TILEY -m
 else
+    echo "Not using mask"
     iss register-to-reference -p $DATAPATH -n $REG_PREFIX -r $ROI -x $TILEX -y $TILEY
 fi
