c519b06ac549755879e8a3b45c51497f495d8008
AlexBecalick <alexander.becalic@crick.ac.uk>
2024-04-30 17:12:28 +0100

    [minor] Add bashrc source for slurm ml fail

--- scripts/segment_mcherry_tile.sh
+++ scripts/segment_mcherry_tile.sh
@@ -4,6 +4,17 @@
 #SBATCH --time=1:00:00
 #SBATCH --mem=16G
 #SBATCH --partition=ncpu
+echo "Starting job $SLURM_JOB_ID"
+echo "Running on $SLURM_JOB_NODELIST"
+echo "Starting register_tile_to_ref.sh"
+echo "Parameters:"
+echo "  DATAPATH: $DATAPATH"
+echo "  ROI: $ROI"
+echo "  TILEX: $TILEX"
+echo "  TILEY: $TILEY"
+
+echo "Loading modules"
+. ~/.bashrc
 ml purge
 
 ml Anaconda3
