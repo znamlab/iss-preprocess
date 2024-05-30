5a3bceca40142355491f52a45be89349c959f4cf
Antonin Blot <antonin.blot@crick.ac.uk>
2024-05-30 18:14:05 +0100

    [feature] project 3d masks

--- iss_preprocess/pipeline/segment.py
+++ iss_preprocess/pipeline/segment.py
@@ -1,16 +1,17 @@
 import warnings
+from tqdm import tqdm
 import glob
 from os import system
 from pathlib import Path
 from znamutils import slurm_it
 import numpy as np
 import pandas as pd
+from scipy.ndimage import binary_erosion, binary_dilation
 from skimage import measure
 from sklearn.preprocessing import StandardScaler
 from sklearn.mixture import GaussianMixture
 from skimage.filters import threshold_triangle
 from skimage.segmentation import expand_labels
-from tqdm import tqdm
 
 import iss_preprocess as iss
 
@@ -107,10 +108,21 @@ def segment_tile(
     )
 
     target = iss.io.get_processed_path(data_path) / "segmentation"
+    target.mkdir(exist_ok=True)
     tile_name = "_".join(map(str, tile_coors))
-    target /= f"{prefix}_masks_{tile_name}.npy"
-    np.save(target, masks)
+    fname = f"{prefix}_masks_{tile_name}.npy"
+    if use_raw_stack:
+        # project masks to a single plane
+        _, _, projected_mask = project_mask(masks, min_pix_size=ops["cellpose_min_pix"])
+        # save raw 3D masks
+        raw_target = target / "raw_masks"
+        raw_target.mkdir(exist_ok=True)
+        np.save(raw_target / fname, masks)
+        masks = projected_mask
+
+    np.save(target / fname, masks)
     print(f"Saved masks to {target}")
+
     return img, masks
 
 
@@ -158,63 +170,138 @@ def get_stack_for_cellpose(data_path, prefix, tile_coors, use_raw_stack=True):
     return img
 
 
-@slurm_it(
-    conda_env="iss-preprocess", slurm_options={"partition": "gpu", "gpus-per-node": 1}
-)
-def run_cell_pos(
-    fname,
-    target,
-    channel,
-    model_type="cyto3",
-    use_gpu=True,
-    flow_threshold=0.4,
-    rescale=0.4,
-    anisotropy=1,
-    min_size=20,
-    do_3D=True,
-    plot=False,
-    save=True,
-):
-    from cellpose.models import CellposeModel
+def project_mask(masks, min_pix_size):
+    """Project masks to a single plane.
 
-    img = iss.io.get_tile_ome(
-        iss.io.get_raw_path(f"{fname}.ome.tif"), use_indexmap=True
-    )[..., channel, :]
-    model = CellposeModel(gpu=use_gpu, model_type=model_type)
-    cellpose_kwargs = dict(
-        rescale=rescale, anisotropy=anisotropy, min_size=min_size, do_3D=do_3D
-    )
-    masks, flows, styles = model.eval(
-        img,
-        channels=[0, 0],
-        tile=True,
-        flow_threshold=flow_threshold,
-        **cellpose_kwargs,
-    )
-    if save:
-        np.savez(target, masks=masks, flows=flows, styles=styles)
-    if plot:
-        import matplotlib.pyplot as plt
+    Args:
+        masks (np.array): 3D array of masks.
+        min_pix_size (int): Minimum number of pixels in the center plane to keep a mask.
 
-        z_middle = img.shape[-1] // 2
-        aspect = img.shape[0] / img.shape[1]
-        fig = plt.figure(figsize=(6, 6 * aspect))
-        ax = fig.add_subplot(1, 1, 1)
-        ax.imshow(img[..., z_middle], vmax=np.nanquantile(img, 0.99), cmap="gray")
-        print(masks.shape)
-        if masks.ndim == 3:
-            masks = np.nanmax(masks, axis=2)
-        ax.contour(
-            masks,
-            linewidths=0.5,
-            alpha=0.5,
-            levels=np.arange(masks.max()),
-            cmap="prism",
-        )
-        txt = " - ".join([f"{k}: {v}" for k, v in cellpose_kwargs.items()])
-        ax.set_title(txt)
-        fig.tight_layout()
-        fig.savefig(target.replace(".npz", ".png"), dpi=1200)
+
+    Returns:
+        np.array: 2D array of projected masks.
+    """
+    print("Separating masks")
+    binary_masks, individual_masks = _separate_masks(masks, min_pix_size=min_pix_size)
+
+    # First merge all the masks that do not overlap
+    n_masks = np.sum(binary_masks, axis=0)
+    projected_mask = np.zeros_like(n_masks)
+    # Find part with overlapping masks
+    overlapping = {}
+    print("Finding overlapping masks")
+    for i_m, m in tqdm(enumerate(binary_masks), total=len(binary_masks)):
+        if n_masks[m].max() == 1:
+            projected_mask[m] = i_m + 1
+        else:
+            overlapping[i_m + 1] = np.unique(individual_masks[:, m])
+
+    print("Merging overlapping masks")
+    for source, to_compare in tqdm(overlapping.items()):
+        to_compare = to_compare[to_compare != 0]
+        # also remove source
+        to_compare = to_compare[to_compare != source]
+        assert len(to_compare) > 0, f"No overlapping masks for {source}"
+        for target in to_compare:
+            projected_mask = _fuse_masks(source, target, binary_masks, projected_mask)
+
+    return binary_masks, individual_masks, projected_mask
+
+
+def _separate_masks(masks, min_pix_size):
+    """Innner function to separate masks and keep only one plane per mask.
+
+    For each mask, find the center plane and keep only that one.
+
+    Args:
+        masks (np.array): 3D array of masks.
+        min_pix_size (int): Minimum number of pixels in the center plane to keep a mask.
+
+    Returns:
+        binary_masks (np.array): 2D array of binary masks.
+        individual_masks (np.array): 2D array of masks with unique values.
+    """
+
+    mask_ids = np.unique(masks[masks != 0])
+
+    # separate masks, keeping only one plane per mask
+    binary_masks = np.zeros((len(mask_ids), *masks.shape[1:]), dtype=bool)
+    individual_masks = np.zeros((len(mask_ids), *masks.shape[1:]), dtype="int16")
+    for i_m, mask_id in tqdm(enumerate(mask_ids), total=len(mask_ids)):
+        # Find the center z plane of the mask
+        mask = masks == mask_id
+        npx_per_plane = np.sum(mask, axis=(1, 2))
+        mask_z = np.where(npx_per_plane > npx_per_plane.max() * 0.3)[0]
+        center_z = mask_z[len(mask_z) // 2]
+        # if the mask is too small, skip it
+        if np.sum(mask[center_z]) < min_pix_size:
+            continue
+        binary_masks[i_m] = mask[center_z]
+        individual_masks[i_m] = mask[center_z] * (i_m + 1)
+    return binary_masks, individual_masks
+
+
+def _fuse_masks(source, target, binary_masks, projected_mask):
+    """Inner function to fuse two masks.
+
+    If the masks barely overlap, we ignore the intersection.
+    If the masks overlap but are not the same, we try to remove the intersection
+    and keep the rest of the mask.
+
+    Args:
+        source (int): ID of the source mask.
+        target (int): ID of the target mask.
+        binary_masks (np.array): 2D array of binary masks.
+        projected_mask (np.array): 2D array of projected masks.
+
+    Returns:
+        np.array: 2D array of projected masks.
+    """
+    s_mask = binary_masks[source - 1].copy()
+    t_mask = binary_masks[target - 1].copy()
+
+    intersection = np.logical_and(s_mask, t_mask)
+    union = np.logical_or(s_mask, t_mask)
+    i_over_u = np.sum(intersection) / np.sum(union)
+    t_in_s = np.sum(intersection) / np.sum(t_mask)
+    s_in_t = np.sum(intersection) / np.sum(s_mask)
+    barely_overlap = i_over_u < 0.01
+    overlap_but_2cells = (i_over_u < 0.6) and (s_in_t < 0.7) and (t_in_s < 0.7)
+    if barely_overlap or overlap_but_2cells:
+        # barely overlapping, we can ignore the intersection
+        s_mask[intersection] = 0
+        t_mask[intersection] = 0
+        # erode and dilate to remove weird shape
+        s_mask = binary_erosion(s_mask, iterations=5)
+        s_mask = binary_dilation(s_mask, iterations=5)
+        t_mask = binary_erosion(t_mask, iterations=5)
+        t_mask = binary_dilation(t_mask, iterations=5)
+
+        projected_mask[s_mask] = source
+        projected_mask[t_mask] = target
+
+    if np.sum(t_mask) < 200:
+        raise ValueError("Too small t")
+    if np.sum(s_mask) < 200:
+        raise ValueError("Too small s")
+    if t_in_s > 0.7:
+        clean_s = s_mask.copy()
+        clean_s[t_mask] = 0
+        clean_s = binary_erosion(clean_s, iterations=5)
+        clean_s = binary_dilation(clean_s, iterations=5)
+        projected_mask[s_mask] = 0
+        projected_mask[clean_s] = source
+        projected_mask[t_mask] = target
+    if s_in_t > 0.7:
+        clean_t = t_mask.copy()
+        clean_t[s_mask] = 0
+        clean_t = binary_erosion(clean_t, iterations=5)
+        clean_t = binary_dilation(clean_t, iterations=5)
+        projected_mask[t_mask] = 0
+        projected_mask[clean_t] = target
+        projected_mask[s_mask] = source
+
+    return projected_mask
 
 
 def segment_roi(
