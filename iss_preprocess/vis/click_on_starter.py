5b43eb28fad206f22b37f93af9391f7019b96a43
Antonin Blot <blota@crick.ac.uk>
2024-06-21 16:27:18 +0100

    [feature] update click on starter

--- iss_preprocess/vis/click_on_starter.py
+++ iss_preprocess/vis/click_on_starter.py
@@ -1,6 +1,8 @@
 import napari
 from tifffile import imread
 import numpy as np
+import pandas as pd
+import matplotlib as mpl
 import iss_preprocess as iss
 from iss_preprocess.pipeline.stitch import load_tile_ref_coors
 
@@ -10,58 +12,114 @@ def load_roi(
     mouse,
     chamber,
     roi,
+    add_hyb=True,
+    add_genes=True,
+    image_to_load=('genes',
+                'hyb',
+                'rab',
+                'reference',
+                'mCherry')
 ):
     """Load one tile in the interactive viewer"""
     viewer = napari.Viewer()
     data_path = f"{project}/{mouse}/{chamber}"
-    metadata = iss.io.load_metadata(data_path)
-    ops = iss.io.load_ops(data_path)
     manual_folder = iss.io.get_processed_path(data_path) / "manual_starter_click"
-    # if rabies exists, also add it
-    rab_f = manual_folder / f"{mouse}_{chamber}_{roi}_rabies.tif"
-    if rab_f.exists():
-        rab = imread(rab_f)
-        print(rab.shape)
-        viewer.add_image(
-            data=[rab[..., 0], rab[::2, ::2, 0], rab[::4, ::4, 0]],
-            name="Rabies",
-            colormap="red",
-            blending="additive",
-        )
-    # if reference exists, add it
-    ref_f = manual_folder / f"{mouse}_{chamber}_{roi}_reference.tif"
-    if ref_f.exists():
-        ref = imread(ref_f)
-        print(ref.shape)
-        viewer.add_image(
-            data=[ref[..., 0], ref[::2, ::2, 0], ref[::4, ::4, 0]],
-            name="Reference",
-            colormap="blue",
-            blending="additive",
-        )
 
-    # add mcherry image
-    mcherry = imread(manual_folder / f"{mouse}_{chamber}_{roi}_mCherry.tif")
-    print(mcherry.shape)
-    for i, col in enumerate(["magenta", "green"]):
-        viewer.add_image(
-            data=[mcherry[..., i], mcherry[::2, ::2, i], mcherry[::4, ::4, i]],
-            name="mCherry",
-            colormap=col,
-            blending="additive",
-        )
+    stuff_to_load = dict(
+                genes="genes_round_1_1",
+                hyb="hybridisation_round_1_1",
+                rab="barcode_round_1_1",
+                reference=ops["reference_prefix"],
+                mCherry="mCherry_1",
+            )
+    stuff_to_load = {k: v for k,v in stuff_to_load.items() if k in image_to_load}
+    chan_color = dict(
+        genes=['inferno'],
+        hyb=['yellow', 'bop blue', 'bop purple', 'bop orange'],
+        rab=['red'],
+        mCherry=['magenta', 'green'],
+        reference=['blue'],
+    )
+    for name, prefix in stuff_to_load.items():
+        fname = manual_folder / f"{mouse}_{chamber}_{roi}_{name}.tif"
+        if not fname.exists():
+            print(f"Skipping {name}. Missing file")
+            continue
+        img = imread(fname)
+        colors = chan_color[name]
+        for ic, col in enumerate(colors):
+            print(f"Adding {name}, ch {ic}")
+            viewer.add_image(
+                data=[img[..., ic], img[::2, ::2, ic], img[::4, ::4, ic]],
+                name=f"{name} - ch {ic}",
+                colormap=col,
+                blending="additive",
+            )
+    
+
+    # add hyb spots
+    if add_hyb:
+        print('Adding hybridisation spots')
+        sp = pd.read_pickle(manual_folder / f"{mouse}_{chamber}_{roi}_hyb_spots.npy")
+        col = dict(Gad1='blue', Vip='green', Slc17a7='red', Sst='orange')
+        for gene, spdf in sp.groupby('gene'):
+            coord = spdf[['y', 'x']].values
+            hyb_points_layer = viewer.add_points(
+                    coord,
+                    face_color=col[gene],
+                    edge_color=[0]*4,
+                    name=f"{gene} spots",
+                    size=10,
+                    opacity=0.9,
+                )
+    
+    
+    # add genes spots
+    if add_genes:
+        print('Adding genes spots')
+        sp = pd.read_pickle(manual_folder / f"{mouse}_{chamber}_{roi}_genes_spots.npy")
+        colors = mpl.colormaps['tab20'].colors
+        # list of all valid symbols
+        symbols = ['arrow', 'clobber', 'cross', 'diamond', 'disc', 'hbar', 'ring',
+            'square', 'star', 'tailed_arrow', 'triangle_down', 'triangle_up', 'vbar', 'x']
+        split_genes = False
+        if split_genes:
+            for ig, (gene, spdf) in enumerate(sp.groupby('gene')):
+                print(f'    adding {gene}')
+                coord = spdf[['y', 'x']].values
+                viewer.add_points(
+                        coord,
+                        face_color=np.repeat([colors[ig%len(colors)]], len(coord), axis=0),
+                        symbol=symbols[ig%len(symbols)],
+                        name=f"{gene} spots",
+                        size=5,
+                        opacity=0.5,
+                    )
+        else:
+            genes = list(sp.gene.unique())
+            gene_id = sp.gene.map(lambda x : genes.index(x)).values
+            face_colors = [colors[g%len(colors)] for g in gene_id]
+            symbol = [symbols[g%len(symbols)] for g in gene_id]
+            coord = sp[['y', 'x']].values
+            viewer.add_points(coord,
+                              face_color=face_colors,
+                              edge_color= [0]*4,
+                              symbol=symbol,
+                              name='Gene spots',
+                              size=10)
 
     # add rabies mask
+    print('Adding rabies masks')
     rab_mask = imread(manual_folder / f"{mouse}_{chamber}_{roi}_rabies_cells_mask.tif")
     if rab_mask.ndim == 3:
         rab_mask = rab_mask[..., 0]
-    print(rab_mask.shape)
     viewer.add_labels(
         data=rab_mask.astype("int32"),
         name="Rabies cells",
     )
 
     # add rabies spots
+    print('Adding rabies spots')
     rab_pts = np.load(
         manual_folder / f"{mouse}_{chamber}_{roi}_rabies_spots.npy", allow_pickle=True
     ).astype(float)
@@ -82,6 +140,7 @@ def load_roi(
     )
 
     # add empty output layer
+    print('Adding output layer')
     mch_points_layer = viewer.add_points(
         [],
         face_color="white",
@@ -91,16 +150,20 @@ def load_roi(
         size=50,
         opacity=0.8,
     )
-
+    print('Running napari')
     napari.run()
 
 
 if __name__ == "__main__":
     project = "becalia_rabies_barseq"
     mouse = "BRAC8498.3e"
-    chamber = "chamber_10"
-    roi = 4
+    chamber = "chamber_08"
+    roi = 3
     data_path = f"{project}/{mouse}/{chamber}"
     ops = iss.io.load_ops(data_path)
-    load_roi(project, mouse, chamber, roi)
+    load_roi(project, mouse, chamber, roi, add_hyb=True, add_genes=True, image_to_load=('genes',
+                'hyb',
+                'rab',
+                'reference',
+                'mCherry'))
     print("Done")
