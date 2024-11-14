aa5e8908f7aa323a775d2b3266fb6a798f22fc5c
Antonin Blot <antonin.blot@crick.ac.uk>
2024-11-14 11:28:45 +0000

    [minor] add colors to import graph

--- docs/dep_graph.py
+++ docs/dep_graph.py
@@ -5,16 +5,24 @@ import pathlib
 
 import graphviz
 from import_deps import ModuleSet
+from matplotlib import cm
 
 # First initialise a ModuleSet instance with a list str of modules to track
 pkg_paths = pathlib.Path("iss_preprocess").glob("**/*.py")
 module_set = ModuleSet([str(p) for p in pkg_paths])
 
 dot = graphviz.Digraph(comment="Relative imports graph of iss_preprocess")
+dot.node_attr.update(style="filled", shape="box")
 
 # first create all the nodes
+colors = list(cm.get_cmap("Set3").colors)
+colors = [f"#{int(r*255):02x}{int(g*255):02x}{int(b*255):02x}" for r, g, b in colors]
+color_dict = {}
 for mod in sorted(module_set.by_name.keys()):
-    dot.node(mod)
+    submod = mod.split(".")[1]
+    if submod not in color_dict:
+        color_dict[submod] = colors.pop(0)
+    dot.node(mod, fillcolor=color_dict[submod])
 
 # then create all the edges
 for mod in sorted(module_set.by_name.keys()):
