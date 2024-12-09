b58f51e0585f12c25ab73840d5aa1e9b114128be
Antonin Blot <antonin.blot@crick.ac.uk>
2024-12-09 18:40:43 +0000

    [feature] plot_zfocus diagnostic func

--- iss_preprocess/diagnostics/__init__.py
+++ iss_preprocess/diagnostics/__init__.py
@@ -7,6 +7,7 @@ but that are not required per se to run it."""
 import matplotlib.pyplot as plt
 from znamutils import slurm_it
 from ..vis.diagnostics import plot_correction_images, plot_tilestats_distributions, plot_spot_sign_image
+from ..vis.utils import plot_matrix_with_colorbar
 from ..io import load_ops, load_stack, get_roi_dimensions, get_processed_path, get_zprofile
 
 
@@ -113,16 +114,14 @@ def check_illumination_correction(
 
 
 def plot_zfocus(data_path, prefix, rois=None, verbose=True):
-    """Plot the z-focus of a ROI
+    """Plot z-profiles and z-focus for a given prefix
 
     Args:
         data_path (str): Relative path to data folder
-        prefix (str): Prefix of the images to load.
-        rois (int | list, optional): ROI to plot. If None, will plot all ROIs. Defaults 
-        to None.
-        verbose (bool, optional): Print info about progress. Defaults to True.
-        
-
+        prefix (str): Prefix of the images to load
+        rois (int | list, optional): List of rois to process. If None, will process all
+            rois. Defaults to None.
+        verbose (bool, optional): Print info about progress. Defaults to True
     """
     roi_dims = get_roi_dimensions(data_path, prefix=prefix)
     if rois is None:
@@ -130,7 +129,7 @@ def plot_zfocus(data_path, prefix, rois=None, verbose=True):
     elif isinstance(rois, int):
         rois = [rois]
 
-    # collect all data    
+    # collect all data
     for roi in rois:
         if verbose:
             print(f"Plotting ROI {roi}")
@@ -146,10 +145,54 @@ def plot_zfocus(data_path, prefix, rois=None, verbose=True):
                     zprofiles = np.zeros((nx, ny, *zprof.shape))
                 zprofiles[tile_x, tile_y, :] = zprof
                 zfocus[tile_x, tile_y, :] = np.argmax(zprof, axis=1)
-        
+
     # find out of focus tiles
-    nz = zfocus.shape[-1]
+    nz = zprofiles.shape[-1]
     correct = (zfocus > 4) & (zfocus < nz - 4)
-    
+
+    # normalise for plots
+    channel_max = np.max(zprofiles, axis=(0, 1, 3))
+    channel_min = np.min(zprofiles, axis=(0, 1, 3))
+    zprofiles = (zprofiles - channel_min[None,None,:,None]) / (channel_max - channel_min)[None,None,:,None]
+    center = nz // 2
     aspect = nx / ny
-    fig, axs = plt.subplots(nx, ny, figsize=(12, 12 * aspect))
+    fig, axs = plt.subplots(nx, ny, figsize=(12, 12 * aspect), sharex=True, sharey=True)
+    for i in range(nx):
+        for j in range(ny):
+            axs[i, j].plot(np.arange(nz)-center, zprofiles[i, j].T)
+            if not np.all(correct[i, j]):
+                axs[i, j].set_title("Out of focus")
+                # also make the border of the axis red
+                for spine in axs[i, j].spines.values():
+                    spine.set_edgecolor("red")
+                    spine.set_linewidth(2)
+    fig.suptitle(f"Z-profiles for {prefix} in ROI {roi}")
+    fig.tight_layout()
+
+    target_folder = get_processed_path(data_path) / "figures" / prefix
+    target_folder.mkdir(exist_ok=True, parents=True)
+    fig.savefig(target_folder / f"zprofile_roi{roi}.png")
+    plt.close(fig)
+
+    # Plot a z focus matrix per channel
+    fig, axs = plt.subplots(2, 2, figsize=(10, 10 * aspect))
+    for i_chan, ax in enumerate(axs.flatten()):
+        plot_matrix_with_colorbar(zfocus[..., i_chan] - center, ax, cmap="coolwarm", vmin=-center, vmax=center)
+        ax.set_title(f"Channel {i_chan}")
+        # Plot a square around each tile which is black if the tile is in focus, red
+        # otherwise and whose width is proportional to amplitude of the z-profile
+        for i in range(nx):
+            for j in range(ny):
+                if correct[i, j, i_chan]:
+                    color = "black"
+                else:
+                    color = "red"
+                ampl = np.max(zprofiles[i, j, :, i_chan]) - np.min(zprofiles[i, j, :, i_chan])
+                rect = plt.Rectangle((j - 0.5, i - 0.5), 1, 1, edgecolor=color, linewidth=0.5 + 2 * ampl, facecolor="White",
+                                     alpha=max(0.5-ampl,0))
+                ax.add_patch(rect)
+
+    fig.suptitle(f"Z-focus for {prefix} in ROI {roi}")
+    fig.tight_layout()
+    fig.savefig(target_folder / f"zfocus_roi{roi}.png")
+    plt.close(fig)
