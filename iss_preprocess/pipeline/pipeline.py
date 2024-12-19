4a7c4bc78823c1399b6382234497451a85331bd4
Antonin Blot <antonin.blot@crick.ac.uk>
2024-12-19 14:26:25 +0000

    [feature] simple sync_and_crunch
    with no projection but jsut Z profile

--- iss_preprocess/pipeline/pipeline.py
+++ iss_preprocess/pipeline/pipeline.py
@@ -1,13 +1,12 @@
 import shlex
 import subprocess
-import time
 import warnings
 from pathlib import Path
 
 import numpy as np
-import yaml
-from tqdm import tqdm
 from znamutils import slurm_it
+
+from iss_preprocess.pipeline.sync_and_crunch import crunch_pos_file
 
 from ..decorators import updates_flexilims
 from ..diagnostics import check_illumination_correction
@@ -44,7 +43,6 @@ from .project import (
     check_projection,
     check_roi_dims,
     project_round,
-    project_tile,
     reproject_failed,
 )
 from .register import (
@@ -75,7 +73,9 @@ __all__ = [
 ]
 
 
-def sync_and_crunch(data_path, source_folder=None, destination_folder=None):
+def sync_and_crunch(
+    data_path, source_folder=None, destination_folder=None, project=False
+):
     """Sync and crunch data from source to destination folder
 
     Args:
@@ -102,117 +102,7 @@ def sync_and_crunch(data_path, source_folder=None, destination_folder=None):
     # sort files by creation date
     position_files = sorted(position_files, key=lambda x: x.stat().st_ctime)
     for pos_file in position_files:
-        crunch_pos_file(data_path, pos_file, destination_folder)
-
-
-def crunch_pos_file(data_path, pos_file, destination_folder=None):
-    """Crunch a single position file
-
-    Args:
-        data_path (str): Relative path to the data folder
-        pos_file (str): Full Path to the position file
-        destination_folder (str, optional): Path to the destination folder. If None,
-            will use the processed folder. Defaults to None.
-    """
-    pos_file = Path(pos_file)
-    assert pos_file.exists(), f"{pos_file} does not exist"
-    assert pos_file.suffix == ".pos", f"{pos_file} is not a .pos file"
-    source_folder = pos_file.parent
-    ops = load_ops(data_path)
-    print(f"Crunching {pos_file.name}", flush=True)
-    # Position files are name like `CHAMBERID_PREFIX_NPOS_positions.pos`
-    # but micromanager adds a _1 or _2 at the end of the prefix later
-    prefix = "_".join(pos_file.name.split("_")[1:-2])
-    print(f"Prefix: {prefix}", flush=True)
-    # Find the prefix folder
-    prefix_folders = []
-    while not prefix_folders:
-        prefix_folders = list(source_folder.glob(f"{prefix}_*"))
-        if not prefix_folders:
-            print(f"No folder found for {prefix}", flush=True)
-            # If the processed folder exists, skip, otherwise wait
-            if (destination_folder / prefix).exists():
-                print(f"{prefix} already processed, skipping", flush=True)
-                return
-            print("Sleeping 20s", flush=True)
-            time.sleep(20)
-
-    for prefix_folder in prefix_folders:
-        # To avoid checking multiple times the same folder, make a "done" file
-        target_pref_folder = destination_folder / prefix_folder.name
-        if not target_pref_folder.exists():
-            target_pref_folder.mkdir(parents=True)
-
-        done_file = target_pref_folder / "DONE"
-        if done_file.exists():
-            print(f"{prefix_folder.name} already processed, skipping", flush=True)
-            continue
-
-        print(f"Looking at {prefix_folder.name}", flush=True)
-        # First build a list of position that should be acquired
-        position_list = []
-        to_project = []
-        with open(pos_file, "r") as f:
-            positions = yaml.safe_load(f)
-        positions = positions["map"]["StagePositions"]["array"]
-        for pos in positions:
-            pos_label = pos["Label"]["scalar"]
-            position_list.append(pos_label)
-            projected_file = f"{target_pref_folder.name}_MMStack_{pos_label}_max.tif"
-            if not (target_pref_folder / projected_file).exists():
-                to_project.append(pos_label)
-        if not to_project:
-            print(f"{prefix_folder.name} already projected, skipping", flush=True)
-            done_file.touch()
-            continue
-
-        print(f"{len(to_project)}/{len(positions)} positions to project", flush=True)
-
-        # Find the raw folder
-        raw_root = get_raw_path("test").parent
-        # Now project the missing positions
-        # Initialize the progress bar
-        pbar = tqdm(total=len(to_project))
-        while len(to_project):
-            pbar.set_description("projecting ...")
-            # check if any new position has been added
-            processed = []
-            for pos in to_project:
-                raw_file = f"{prefix_folder.name}_MMStack_{pos}"
-                raw_file = list(prefix_folder.glob(f"{raw_file}*.tif"))
-                if len(raw_file):
-                    assert len(raw_file) == 1, f"Multiple files found for {raw_file}"
-                    raw_file = raw_file[0]
-                    fname = str(raw_file.relative_to(raw_root)).replace(".ome.tif", "")
-                    project_tile(fname, ops, overwrite=False, sth=13, target_name=None)
-                    processed.append(pos)
-                    pbar.update(1)
-            # Remove processed positions from the list out of the loop
-            for done in processed:
-                to_project.remove(done)
-            if len(to_project):
-                pbar.set_description("waiting for new positions...")
-                time.sleep(1)
-        pbar.close()
-        print(f"Done projecting {prefix_folder.name}", flush=True)
-
-        # Plot diagnostic Z-stacks
-        print(f"Plotting diagnostic Z-stacks for {prefix_folder.name}", flush=True)
-
-        # Plot overview images
-        print(f"Plotting overview images for {prefix_folder.name}", flush=True)
-        plot_overview_images(
-            data_path,
-            prefix_folder.name,
-            plot_grid=True,
-            downsample_factor=25,
-            save_raw=False,
-            dependency=None,
-        )
-
-        # Save the done file
-        done_file.touch()
-        print(f"Done processing {prefix_folder}", flush=True)
+        crunch_pos_file(data_path, pos_file, destination_folder, project=project)
 
 
 @slurm_it(conda_env="iss-preprocess")
---
+++ iss_preprocess/pipeline/sync_and_crunch.py
@@ -0,0 +1,170 @@
+import time
+from pathlib import Path
+
+import numpy as np
+import yaml
+from tqdm import tqdm
+
+from ..diagnostics.diag_stitching import plot_overview_images
+from ..io import get_processed_path, get_raw_path, get_tile_ome, load_ops
+from .project import project_tile
+
+
+def crunch_pos_file(data_path, pos_file, destination_folder=None, project=False):
+    """Crunch a single position file
+
+    Args:
+        data_path (str): Relative path to the data folder
+        pos_file (str): Full Path to the position file
+        destination_folder (str, optional): Path to the destination folder. If None,
+            will use the processed folder. Defaults to None.
+    """
+    pos_file = Path(pos_file)
+    assert pos_file.exists(), f"{pos_file} does not exist"
+    assert pos_file.suffix == ".pos", f"{pos_file} is not a .pos file"
+    source_folder = pos_file.parent
+    ops = load_ops(data_path)
+    print(f"Crunching {pos_file.name}", flush=True)
+    # Position files are name like `CHAMBERID_PREFIX_NPOS_positions.pos`
+    # but micromanager adds a _1 or _2 at the end of the prefix later
+    prefix = "_".join(pos_file.name.split("_")[1:-2])
+    print(f"Prefix: {prefix}", flush=True)
+    # Find the prefix folder
+    prefix_folders = []
+    while not prefix_folders:
+        prefix_folders = list(source_folder.glob(f"{prefix}_*"))
+        if not prefix_folders:
+            print(f"No folder found for {prefix}", flush=True)
+            # If the processed folder exists, skip, otherwise wait
+            if (destination_folder / prefix).exists():
+                print(f"{prefix} already processed, skipping", flush=True)
+                return
+            print("Sleeping 20s", flush=True)
+            time.sleep(20)
+
+    for prefix_folder in prefix_folders:
+        # To avoid checking multiple times the same folder, make a "done" file
+        target_pref_folder = destination_folder / prefix_folder.name
+        if not target_pref_folder.exists():
+            target_pref_folder.mkdir(parents=True)
+
+        done_file = target_pref_folder / "DONE"
+        if done_file.exists():
+            print(f"{prefix_folder.name} already processed, skipping", flush=True)
+            continue
+
+        print(f"Looking at {prefix_folder.name}", flush=True)
+        # First build a list of position that should be acquired
+        position_list = []
+        to_project = []
+        with open(pos_file, "r") as f:
+            positions = yaml.safe_load(f)
+        positions = positions["map"]["StagePositions"]["array"]
+        for pos in positions:
+            pos_label = pos["Label"]["scalar"]
+            position_list.append(pos_label)
+            if project:
+                projected_file = (
+                    f"{target_pref_folder.name}_MMStack_{pos_label}_max.tif"
+                )
+            else:
+                projected_file = (
+                    f"{target_pref_folder.name}_MMStack_{pos_label}_zprofile.npz"
+                )
+            if not (target_pref_folder / projected_file).exists():
+                to_project.append(pos_label)
+        if not to_project:
+            print(f"{prefix_folder.name} already projected, skipping", flush=True)
+            done_file.touch()
+            continue
+
+        print(f"{len(to_project)}/{len(positions)} positions to project", flush=True)
+
+        # Find the raw folder
+        raw_root = get_raw_path("test").parent
+        # Now project the missing positions
+        # Initialize the progress bar
+        pbar = tqdm(total=len(to_project))
+        while len(to_project):
+            pbar.set_description("projecting ...")
+            # check if any new position has been added
+            processed = []
+            for pos in to_project:
+                raw_file = f"{prefix_folder.name}_MMStack_{pos}"
+                raw_file = list(prefix_folder.glob(f"{raw_file}*.tif"))
+                if len(raw_file):
+                    assert len(raw_file) == 1, f"Multiple files found for {raw_file}"
+                    raw_file = raw_file[0]
+                    fname = str(raw_file.relative_to(raw_root)).replace(".ome.tif", "")
+                    if project:
+                        project_tile(fname, ops, overwrite=False)
+                    else:
+                        quick_std(fname, overwrite=False, window_size=500)
+                    processed.append(pos)
+                    pbar.update(1)
+            # Remove processed positions from the list out of the loop
+            for done in processed:
+                to_project.remove(done)
+            if len(to_project):
+                pbar.set_description("waiting for new positions...")
+                time.sleep(1)
+        pbar.close()
+        print(f"Done projecting {prefix_folder.name}", flush=True)
+
+        # Plot diagnostic Z-stacks
+        print(f"Plotting diagnostic Z-stacks for {prefix_folder.name}", flush=True)
+        print("TODO!!!!!!!!!!!!!!!!!!!!!!!!")
+
+        if project:
+            # Plot overview images
+            print(f"Plotting overview images for {prefix_folder.name}", flush=True)
+            plot_overview_images(
+                data_path,
+                prefix_folder.name,
+                plot_grid=True,
+                downsample_factor=25,
+                save_raw=False,
+                dependency=None,
+            )
+
+        # Save the done file
+        done_file.touch()
+        print(f"Done processing {prefix_folder}", flush=True)
+
+
+def quick_std(fname, overwrite=False, window_size=500):
+    """Calculate the standard deviation of a small part of the image.
+
+    To make it faster, we only take a small window around the center of the image.
+
+    Args:
+        fname (str): Name of the file to process.
+        overwrite (bool, optional): If True, will overwrite the existing file. Defaults
+            to False.
+        window_size (int, optional): Size of the window to take around the center
+            of the image. Defaults to 500.
+
+    Returns:
+        np.array: standard deviation along the Z axis.
+    """
+    target_name = get_processed_path(fname + "_zprofile.npz")
+    if not overwrite and target_name.exists():
+        return
+    im = get_tile_ome(
+        get_raw_path(fname + ".ome.tif"),
+        get_raw_path(fname + "_metadata.txt"),  # note that this won't be used
+        # if use_indexmap is True
+        use_indexmap=True,
+    )
+    get_processed_path(fname).parent.mkdir(parents=True, exist_ok=True)
+
+    # To check if the focus was correct, we also save a small projectiong along Z
+    center = np.array(im.shape[:2]) // 2
+    part = im[
+        center[0] - window_size : center[0] + window_size,
+        center[1] - window_size : center[1] + window_size,
+    ]
+    std_z = np.std(part, axis=(0, 1))
+    np.savez(target_name, std=std_z)
+
+    return np.std(part, axis=(0, 1))
