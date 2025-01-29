ae579177ebd316e0983e40a6ccd80fbecbb6843b
Antonin Blot <antonin.blot@crick.ac.uk>
2025-01-29 15:47:00 +0000

    [dev] add __version__

---
+++ iss_preprocess/__init__.py
@@ -0,0 +1,7 @@
+from importlib.metadata import PackageNotFoundError, version
+
+try:
+    __version__ = version("iss_preprocess")
+except PackageNotFoundError:
+    # package is not installed
+    pass
