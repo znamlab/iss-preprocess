33a77c1eca4afde85fe19c17f4bba017ea7057ea
DavidNEssenbaek <david.essenbaek@crick.ac.uk>
2026-03-15 21:54:31 +0000

    [feature] Expose main submodules for iss.<submodule> usage

--- iss_preprocess/__init__.py
+++ iss_preprocess/__init__.py
@@ -6,4 +6,15 @@ except PackageNotFoundError:
     # package is not installed
     pass
 
+# Expose main submodules for iss.<submodule> usage
 from . import io
+from . import call
+from . import cli
+from . import config
+from . import coppafish
+from . import diagnostics
+from . import image
+from . import pipeline
+from . import reg
+from . import segment
+from . import vis
